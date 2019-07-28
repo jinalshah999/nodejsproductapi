08ab21e758005110ea24967ab16603e37b481d64
jinalshah999 <jinalshah999@gmail.com>
2019-07-28 13:05:31 +0530

    init commit

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
-var mysql=require('mysql');
-var connection=mysql.createPool({
-host:'localhost',
-     user:'root',
-    password:'',
-    database:'demo'
-  
+var mysql = require('mysql');
+var connection = mysql.createPool({
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'demo'
+
+    host: 'JINAL.db.9340728.c20.hostedresource.net',
+    user: 'JINAL',
+    password: 'Angular@9797',
+    database: 'JINAL'
+
 });
 
-module.exports=connection;
+
+module.exports = connection;
